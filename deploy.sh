d66fb819c00c04290bb8b6f31d7d5f3abfe9b4dc
Devin Matte <devinmatte@gmail.com>
2024-02-23 14:38:06 -0500

    API Hosting, prod setup, and calendar generation (#10)
    * Fixing localhost and prod hosting
    
    * Adding Calendar endpoint
    
    * Adding line color to event

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -x
 
+set -e
+
+export AWS_PROFILE=transitmatters
+export AWS_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_PAGER=""
+
 if [[ -z "$DD_API_KEY" || -z "$TM_LABS_WILDCARD_CERT_ARN" ]]; then
     echo "Must provide DD_API_KEY and TM_LABS_WILDCARD_CERT_ARN in environment" 1>&2
     exit 1
 fi
 
 STACK_NAME=shutdown-tracker
+CHALICE_STAGE=production
+
 FRONTEND_HOSTNAME="shutdowns.labs.transitmatters.org"
 FRONTEND_ZONE="labs.transitmatters.org"
-BUCKET="$FRONTEND_HOSTNAME"
+FRONTEND_BUCKET="$FRONTEND_HOSTNAME"
 FRONTEND_CERT_ARN="$TM_LABS_WILDCARD_CERT_ARN" 
+
+BACKEND_HOSTNAME="shutdowns-api.labs.transitmatters.org"
+BACKEND_ZONE="labs.transitmatters.org"
+BACKEND_BUCKET=shutdown-tracker-backend
+BACKEND_CERT_ARN="$TM_LABS_WILDCARD_CERT_ARN"
 
 # Identify the version and commit of the current deploy
 GIT_VERSION=`git describe --tags --always`
@@ -22,14 +36,23 @@ DD_TAGS="git.commit.sha:$GIT_SHA,git.repository_url:github.com/transitmatters/sh
 npm run build
 
 # Deploy to cloudformation
-aws cloudformation deploy --template-file cloudformation.json --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --parameter-overrides \
+pushd server/ > /dev/null
+poetry export --without-hashes --output requirements.txt
+poetry run chalice package --stage $CHALICE_STAGE --merge-template cloudformation.json cfn/
+aws cloudformation package --template-file cfn/sam.json --s3-bucket $BACKEND_BUCKET --output-template-file cfn/packaged.yaml
+aws cloudformation deploy --template-file cfn/packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --parameter-overrides \
     TMFrontendHostname=$FRONTEND_HOSTNAME \
     TMFrontendZone=$FRONTEND_ZONE \
     TMFrontendCertArn=$FRONTEND_CERT_ARN \
+    TMBackendCertArn=$BACKEND_CERT_ARN \
+    TMBackendHostname=$BACKEND_HOSTNAME \
+    TMBackendZone=$BACKEND_ZONE \
     DDApiKey=$DD_API_KEY \
     GitVersion=$GIT_VERSION \
     DDTags=$DD_TAGS
-aws s3 sync dist/ s3://$BUCKET
+
+popd > /dev/null
+aws s3 sync dist/ s3://$FRONTEND_BUCKET
 
 # Grab the cloudfront ID and invalidate its cache
 CLOUDFRONT_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items!=null] | [?contains(Aliases.Items, '$FRONTEND_HOSTNAME')].Id | [0]" --output text)
--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+from icalendar import Calendar, Event, vCalAddress
 import json
 import os
-from chalice import Chalice, CORSConfig, ConvertToMiddleware
+from chalice import Chalice, CORSConfig, ConvertToMiddleware, Response
 from datadog_lambda.wrapper import datadog_lambda_wrapper
 import requests
 from urllib.parse import urlencode
@@ -12,8 +14,9 @@ TM_CORS_HOST = os.environ.get("TM_CORS_HOST", localhost)
 
 if TM_CORS_HOST != localhost:
     app.register_middleware(ConvertToMiddleware(datadog_lambda_wrapper))
-    
-cors_config = CORSConfig(allow_origin=f"http://{TM_CORS_HOST}", max_age=3600)
+    cors_config = CORSConfig(allow_origin=f"https://{TM_CORS_HOST}", max_age=3600)
+else:
+    cors_config = CORSConfig(allow_origin="*", max_age=3600)
 
 
 @app.route("/api/aggregate/{method}", cors=cors_config)
@@ -24,3 +27,31 @@ def proxy(method):
         )
         return json.dumps(data_dashboard_response.json(), indent=4, sort_keys=True, default=str)
     return None
+
+@app.route("/calendar", cors=cors_config)
+def calendar():
+    # init the calendar
+    cal = Calendar()
+    cal.add('prodid', '-//MBTA Shutdown Calendar//transitmatters.org//')
+    cal.add('version', '2.0')
+
+    # get the shutdowns
+    with open("../src/constants/shutdowns.json", "r") as f:
+        shutdowns = json.load(f)
+        lines = shutdowns.keys()
+        for line in lines:
+            for shutdown in shutdowns[line]:
+                event = Event()
+                event_name = f"MBTA {line} Line Shutdown {shutdown['start_station']} - {shutdown['end_station']}"
+                event.add('name', event_name)
+                event.add('summary', event_name)
+                event.add('description', f"The MBTA {line} Line will be shut down between {shutdown['start_station']} and {shutdown['end_station']}. During this time the MBTA plans to make repairs and improvements to the line.")
+                event.add('dtstart', datetime.strptime(shutdown['start_date'], "%Y-%m-%d").date())
+                event.add('dtend', datetime.strptime(shutdown['stop_date'], "%Y-%m-%d").date())
+                event.add('color', line)
+
+                cal.add_component(event)
+
+    response = Response(body=cal.to_ical())
+    response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
+    return response
