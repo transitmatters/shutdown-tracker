2524987245d405cc9ee18194a32ea0d5e3124419
Devin Matte <devinmatte@gmail.com>
2024-02-24 16:39:37 -0500

    Adding detailed README and cleanup

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from icalendar import Calendar, Event, vCalAddress
+from icalendar import Calendar, Event
 import json
 import os
 from chalice import Chalice, CORSConfig, ConvertToMiddleware, Response
@@ -21,19 +21,20 @@ else:
 
 @app.route("/api/aggregate/{method}", cors=cors_config)
 def proxy(method):
-    if (method is not None):
+    if method is not None:
         data_dashboard_response = requests.get(
             f"https://dashboard-api.labs.transitmatters.org/api/aggregate/{method}?{urlencode(app.current_request.query_params)}"
         )
         return json.dumps(data_dashboard_response.json(), indent=4, sort_keys=True, default=str)
     return None
 
+
 @app.route("/calendar", cors=cors_config)
 def calendar():
     # init the calendar
     cal = Calendar()
-    cal.add('prodid', '-//MBTA Shutdown Calendar//transitmatters.org//')
-    cal.add('version', '2.0')
+    cal.add("prodid", "-//MBTA Shutdown Calendar//transitmatters.org//")
+    cal.add("version", "2.0")
 
     # get the shutdowns
     with open("../src/constants/shutdowns.json", "r") as f:
@@ -43,12 +44,15 @@ def calendar():
             for shutdown in shutdowns[line]:
                 event = Event()
                 event_name = f"MBTA {line} Line Shutdown {shutdown['start_station']} - {shutdown['end_station']}"
-                event.add('name', event_name)
-                event.add('summary', event_name)
-                event.add('description', f"The MBTA {line} Line will be shut down between {shutdown['start_station']} and {shutdown['end_station']}. During this time the MBTA plans to make repairs and improvements to the line.")
-                event.add('dtstart', datetime.strptime(shutdown['start_date'], "%Y-%m-%d").date())
-                event.add('dtend', datetime.strptime(shutdown['stop_date'], "%Y-%m-%d").date())
-                event.add('color', line)
+                event.add("name", event_name)
+                event.add("summary", event_name)
+                event.add(
+                    "description",
+                    f"The MBTA {line} Line will be shut down between {shutdown['start_station']} and {shutdown['end_station']}. During this time the MBTA plans to make repairs and improvements to the line.",
+                )
+                event.add("dtstart", datetime.strptime(shutdown["start_date"], "%Y-%m-%d").date())
+                event.add("dtend", datetime.strptime(shutdown["stop_date"], "%Y-%m-%d").date())
+                event.add("color", line)
 
                 cal.add_component(event)
 
