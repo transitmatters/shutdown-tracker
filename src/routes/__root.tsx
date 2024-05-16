b12d048911f85e9f6fda2730ad73a357e6b3ae37
Seth Kaplan <sethkaplan73@gmail.com>
2024-05-16 14:37:35 -0400

    feat: remove tanstack devtools (#39)
    * remove tanstack devtools
    
    * add package lock

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
 });
