25ba7457331df5fc482f0808940c82c7c03474e7
Tyler Kindy <tylerkindy@gmail.com>
2025-02-22 08:44:19 -0500

    Add check mode to sort script

--- sort-shutdowns.py
+++ sort-shutdowns.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 def build_sort_key(shutdown):
@@ -18,6 +19,14 @@ new_data = {}
 for key in data:
     new_data[key] = sorted(data[key], key=build_sort_key)
 
-with open("src/constants/shutdowns.json", "w") as f:
-    json.dump(new_data, f, indent=2)
-    f.write("\n")
+if "--check" in sys.argv:
+    if data == new_data:
+        print("shutdowns.json is sorted")
+        sys.exit(0)
+
+    print("shutdowns.json is not sorted. Run `python sort-shutdowns.py` to sort it.")
+    sys.exit(1)
+else:
+    with open("src/constants/shutdowns.json", "w") as f:
+        json.dump(new_data, f, indent=2)
+        f.write("\n")
